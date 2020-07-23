be8ae1bfc71631927521ffe17bec4d9aaaa9303f
Alex Licu <57290562+AlexLicu@users.noreply.github.com>
2020-07-23 16:40:17 +0300

    Add files via upload

---
+++ criminal_russia/fxmanifest.lua
@@ -0,0 +1,15 @@
+
+description "Criminal Russia map mod converted for FiveM"
+author 'Mishka Volkav, Dekurwinator, CR Team, Iceman/Licu'
+version '1.0.0'
+
+fx_version 'bodacious'
+games { 'gta5' }
+
+this_is_a_map 'yes'
+files {
+	'gtxd.meta',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/data/crim_russia.ytyp'
+data_file 'GTXD_PARENTING_DATA' 'gtxd.meta'
